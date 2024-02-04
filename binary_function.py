47ff4450011bdc310be4f0f2b207cff47ec9dc44
Vsevolod Vaskin <vaskin.2003@gmail.com>
2024-02-04 20:14:49 +0200

    Create BinaryFunction class and fix bugs

---
+++ binary_function.py
@@ -0,0 +1,129 @@
+import itertools
+from collections import defaultdict
+
+
+class BinaryFunction:
+    def __init__(self, truth_tables):
+        assert all(len(table) == len(truth_tables[0]) for table in truth_tables)
+        self.truth_tables = truth_tables
+
+    @property
+    def inputs(self):
+        return len(self.truth_tables[0]).bit_length() - 1
+
+    @property
+    def outputs(self):
+        return len(self.truth_tables)
+
+    def __call__(self, args: str) -> str:
+        assert len(args) == self.inputs
+        idx = int(args, 2)
+        return ''.join(t[idx] for t in self.truth_tables)
+
+    def __hash__(self):
+        return hash(self.truth_tables)
+
+    def __eq__(self, other):
+        assert isinstance(other, BinaryFunction)
+        return self.truth_tables == other.truth_tables
+
+    def __repr__(self):
+        return ' '.join(self.truth_tables)
+
+    def __lt__(self, other):
+        assert isinstance(other, BinaryFunction)
+        return self.truth_tables < other.truth_tables
+
+    def transform(self, input_permutation=None, input_negotiations=None, output_negotiations=None):
+
+        assert input_permutation is None or self.inputs == len(input_permutation)
+        assert input_negotiations is None or self.inputs == len(input_negotiations)
+        assert output_negotiations is None or self.outputs == len(output_negotiations)
+
+        result_tables = tuple(list() for _ in range(len(self.truth_tables)))
+
+        # Iterate over all possible arguments
+        for i in range(1 << self.inputs):
+            args = format(i, f'0{self.inputs}b')
+
+            # Apply inputs permutation
+            if input_permutation is None:
+                permuted_args = args
+            else:
+                permuted_args = ''.join(args[input_permutation[j]] for j in range(self.inputs))
+
+            # Apply inputs negotiations
+            if input_negotiations is None:
+                negotiated_args = permuted_args
+            else:
+                negotiated_args = ''.join('0' if (permuted_args[j] == '0') ^ input_negotiations[j] else '1'
+                                          for j in range(self.inputs))
+
+            # Iterate over functions values
+            for j, (table, val) in enumerate(zip(result_tables, self(negotiated_args))):
+                # Apply output negotiation
+                if output_negotiations is not None and output_negotiations[j]:
+                    val = '1' if val == '0' else '0'
+
+                # Save value
+                table.append(val)
+
+        return BinaryFunction(tuple(sorted(''.join(table) for table in result_tables)))
+
+    def normalized(self):
+        new_truth_tables = []
+        for truth_table in self.truth_tables:
+            if truth_table[0] == '0':
+                new_truth_tables.append(truth_table)
+            else:
+                new_truth_tables.append(''.join('1' if x == '0' else '0' for x in truth_table))
+        return BinaryFunction(tuple(sorted(new_truth_tables)))
+
+    def get_equivalence_class(self, basis):
+        assert basis == 'aig' or basis == 'bench'
+
+        function_class = set()
+
+        # Iterate over input permutations
+        for permutation in itertools.permutations(range(self.inputs)):
+            assert isinstance(permutation, tuple)
+
+            if basis == 'bench':
+                # Find candidate for bench without input negotiations
+                candidate = self.transform(permutation).normalized()
+                function_class.add(candidate)
+            else:
+                assert basis == 'aig'
+                # Iterate over all input negotiations (for AIG)
+                for input_negotiations in itertools.product([False, True], repeat=self.inputs):
+                    candidate = self.transform(permutation, input_negotiations).normalized()
+                    function_class.add(candidate)
+
+        return function_class
+
+    @staticmethod
+    def all_functions(inputs, outputs):
+        for tables in itertools.combinations(itertools.product('01', repeat=2 ** inputs), outputs):
+            tables = tuple(map(lambda x: ''.join(x), tables))
+            yield BinaryFunction(tables)
+
+    @staticmethod
+    def get_representative(functions):
+        return min(functions)
+
+    @staticmethod
+    def all_functions_grouped(inputs, outputs, basis):
+        groups = defaultdict(set)
+        functions_to_be_enumerated = set(BinaryFunction.all_functions(inputs, outputs))
+        for function in BinaryFunction.all_functions(inputs, outputs):
+            if function not in functions_to_be_enumerated:
+                continue
+            function_group = function.get_equivalence_class(basis)
+            representative = BinaryFunction.get_representative(function_group)
+            groups[representative] = function_group
+            functions_to_be_enumerated.difference_update(function_group)
+        return groups
+
+    @staticmethod
+    def all_normalized_functions(inputs, outputs):
+        raise NotImplementedError()
--- generate_all_functions.py
+++ generate_all_functions.py
@@ -1,96 +1,6 @@
 import itertools
 import sys
-from collections import defaultdict
-
-
-def eval_functions(truth_tables, args):
-    idx = int(args, 2)
-    return tuple(t[idx] for t in truth_tables)
-
-
-def get_transformed_function(number_of_inputs, truth_tables, permutation, input_negotiations, output_negotiations):
-    assert number_of_inputs == len(input_negotiations)
-    assert len(truth_tables) == len(output_negotiations)
-
-    result_tables = tuple(list() for _ in range(len(truth_tables)))
-
-    # Iterate over all possible arguments
-    for i in range(1 << number_of_inputs):
-        args = format(i, f'0{number_of_inputs}b')
-
-        # Apply inputs permutation
-        permuted_args = ''.join(args[permutation[j]] for j in range(number_of_inputs))
-
-        # Apply inputs negotiations
-        negotiated_args = ''.join('0' if (permuted_args[j] == '0') ^ input_negotiations[j] else '1'
-                                  for j in range(number_of_inputs))
-
-        # Iterate over functions values
-        for j, (table, val) in enumerate(zip(result_tables, eval_functions(truth_tables, negotiated_args))):
-            # Apply output negotiation
-            if output_negotiations[j]:
-                val = '1' if val == '0' else '0'
-
-            # Save value
-            table.append(val)
-
-    return tuple(sorted(''.join(table) for table in result_tables))
-
-
-def normalize_function(number_of_inputs, truth_tables, basis):
-    class_representative = truth_tables
-
-    def normalize_outputs(tables):
-        new_tables = []
-        for table in tables:
-            if table[0] == '0':
-                new_tables.append(table)
-            else:
-                new_tables.append(''.join('1' if x == '0' else '0' for x in table))
-        return tuple(new_tables)
-
-    # Iterate over input permutations
-    for permutation in itertools.permutations(range(number_of_inputs)):
-        assert isinstance(permutation, tuple)
-
-        if basis == 'bench':
-            # Find candidate for bench without input negotiations
-            candidate = get_transformed_function(number_of_inputs, truth_tables, permutation,
-                                                 [False] * number_of_inputs, [False] * len(truth_tables))
-
-            class_representative = min(class_representative, normalize_outputs(candidate))
-        else:
-            assert basis == 'aig'
-            # Iterate over all input negotiations (for AIG)
-            for input_negotiations in itertools.product([False, True], repeat=number_of_inputs):
-                candidate = get_transformed_function(number_of_inputs, truth_tables, permutation,
-                                                     input_negotiations, [False] * len(truth_tables))
-                class_representative = min(class_representative, normalize_outputs(candidate))
-
-    return class_representative
-
-
-def get_all_functions(inputs, outputs):
-    for tables in itertools.combinations(itertools.product('01', repeat=2 ** inputs - 1), outputs):
-        tables = tuple(map(lambda x: '0' + ''.join(x), tables))
-        yield tables
-
-
-def normalized_truth_tables(inputs, outputs, basis):
-    for tables in get_all_functions(inputs, outputs):
-        normalized = normalize_function(inputs, tables, basis)
-        if normalized == tables:
-            yield tables
-
-
-def get_grouped_functions(inputs, outputs, basis):
-    grouped_functions = defaultdict(list)
-
-    for truth_table in get_all_functions(inputs, outputs):
-        normalized = normalize_function(inputs, truth_table, basis)
-        grouped_functions[normalized].append(truth_table)
-
-    return grouped_functions
+from binary_function import BinaryFunction
 
 
 def main():
@@ -100,8 +10,8 @@ def main():
 
     assert basis == 'aig' or basis == 'bench'
 
-    for truth_tables in normalized_truth_tables(number_of_inputs, number_of_outputs, basis):
-        print(*truth_tables)
+    groups = BinaryFunction.all_functions_grouped(number_of_inputs, number_of_outputs, basis)
+    print(*groups.keys(), sep='\n')
 
 
 if __name__ == '__main__':
